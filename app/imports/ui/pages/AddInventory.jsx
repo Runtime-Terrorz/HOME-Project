50930be17265a6b87e21853c9ffe790d2e03681f
acatarinaoaraujo <acoa_@hotmail.com>
2021-09-07 10:53:55 -1000

    inventory form schema

--- app/imports/ui/pages/AddInventory.jsx
+++ app/imports/ui/pages/AddInventory.jsx
@@ -1,74 +1,73 @@
 import React from 'react';
-import { Grid, Segment, Header, Form /*, Loader */ } from 'semantic-ui-react';
-import { /* AutoForm ,*/ TextField, SubmitField } from 'uniforms-semantic';
-/* import swal from 'sweetalert';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { Grid, Segment, Header, Form } from 'semantic-ui-react';
+import { AutoForm, ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
+import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
-import { _ } from 'meteor/underscore';
-import { withTracker } from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types'; */
+import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
+import SimpleSchema from 'simpl-schema';
+import { stuffDefineMethod } from '../../api/stuff/StuffCollection.methods';
 
-// const bridge = new SimpleSchema2Bridge(ADD SCHEMA NAME HERE.schema);
+/** Create a schema to specify the structure of the data to appear in the form. */
+const formSchema = new SimpleSchema({
+  name: String,
+  reserves: Number,
+  storage: {
+    type: String,
+    allowedValues: ['Case 1', 'Case 2', 'Case 3', 'Case 4', 'Case 5', 'Case 6', 'Case 7',
+      'Case 8', 'Refrigerator Closet', 'Bottom Drawer', 'Freezer', 'Freezer-Derm', 'Drawer 2-2',
+      'Drawer 2-3', 'Emergency Kit', 'Refrigerator'],
+    defaultValue: 'Case 1',
+  },
+    lot: String,
+    expiration: String,
+});
 
-/** Renders the AddUser Page: what appears after the user logs in. */
+/** Renders the Page for adding stuff. */
 class AddInventory extends React.Component {
 
   /** On submit, insert the data. */
-  /*submit(data) {
-    Meteor.call(updateProfileMethod, data, (error) => {
-      if (error) {
-        swal('Error', error.message, 'error');
-      } else {
-        swal('Success', 'Medication added successfully', 'success');
-      }
-    });
-  }*/
-
-  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
-  render() {
-    /*return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  submit(data, formRef) {
+    // console.log('AddInventory.submit', data);
+    const { name, reserves, storage, lot, expiration } = data;
+    const owner = Meteor.user().username;
+    // console.log(`{ ${name}, ${quantity}, ${condition}, ${owner} }`);
+    stuffDefineMethod.call({ name, reserves, storage, lot, expiration, owner },
+        (error) => {
+          if (error) {
+            swal('Error', error.message, 'error');
+            // console.error(error.message);
+          } else {
+            swal('Success', 'Item added successfully', 'success');
+            formRef.reset();
+            // console.log('Success');
+          }
+        });
   }
 
-  renderPage() { */
-    // const email = Meteor.user().username;
-    // const profile = ADD SCHEMA HERE.collection.findOne({ email });
-    // const model = _.extend({}, profile);
+  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  render() {
+    let fRef = null;
     return (
-        <div className="container-addmed">
-          <Grid container centered>
-            <Grid.Column width={8}>
-                <Header as="h1" textAlign="center"><em>Add a Medication</em></Header>
-                <Segment>
-                  <Form.Group>
-                    <TextField id='name' name='name' showInlineError={true} placeholder={'Name of Medication'}/>
-                  </Form.Group>
-                  <Form.Group widths={'equal'}>
-                  <TextField id='reserves' name='reserves' showInlineError={true} placeholder={'Reserves'}/>
-                  <TextField id='location' name='location' showInlineError={true} placeholder={'Storage Location'}/>
-                  </Form.Group>
-                  <Form.Group widths={'equal'}>
-                  <TextField id='lot' name='lot' showInlineError={true} placeholder={'Lot Number #'}/>
-                  <TextField id='expiration'  name='expiration' showInlineError={true} placeholder={'Expiration Date'} />
-                  </Form.Group>
-                  <SubmitField id='Addmed-submit' value='Submit'/>
-                </Segment>
-            </Grid.Column>
-          </Grid>
-        </div>
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" textAlign="center">Add New Medication</Header>
+            <AutoForm ref={ref => { fRef = ref; }} schema={formSchema} onSubmit={data => this.submit(data, fRef)} >
+              <Segment>
+                <TextField name='name' placeholder={'Ex: Acetaminophen 500 mg Caps'}/>
+                <SelectField name='storage'/>
+                <Form.Group widths={'equal'}>
+                  <NumField name='reserves' decimal={false} placeholder={'The total quantity to be added'}/>
+                  <TextField name='lot' placeholder={'Ex: #133258'}/>
+                  <TextField name='expiration' placeholder={'DD/MM/YYYY'}/>
+                </Form.Group>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid>
     );
   }
 }
-
-/*AddUser.propTypes = {
-  ready: PropTypes.bool.isRequired,
-};*/
-
-/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-/*export default withTracker(() => {
-  const sub1 = Meteor.subscribe(Profiles.userPublicationName);
-  return {
-    ready: sub1.ready(),
-  };
-})(AddUser);*/
 
 export default AddInventory;
