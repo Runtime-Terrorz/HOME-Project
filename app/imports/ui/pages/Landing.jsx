bc59319a11c078eb6ddb1d6e70f5c5c4fcd99089
acatarinaoaraujo <acoa_@hotmail.com>
2021-09-16 10:38:00 -1000

    update landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Menu } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+        <Grid style={{ marginTop: '50px' }} verticalAlign='middle' textAlign='center' container>
+          <Menu>
+          <Grid.Column width={5}>
+            <Menu.Item as={NavLink} activeClassName="" exact to="add">
+            <Image width={'300px'} src="/images/addorder.png"/>
+            </Menu.Item>
           </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
+            <Grid.Column width={5}>
+              <Menu.Item as={NavLink} activeClassName="" exact to="inventory">
+                <Image width={'280px'} src="/images/listinventory.png"/>
+              </Menu.Item>
+            </Grid.Column>
+        </Menu>
         </Grid>
     );
   }
